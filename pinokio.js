ae607d8d4f2c80d74960ca15bf6804aa0b2aa1bb
Frederic Brenner <fredericbrenner98@gmx.de>
2026-03-05 18:11:53 +0100

    fix installer for new pinokio version

--- pinokio.js
+++ pinokio.js
@@ -1,46 +1,82 @@
-const os = require('os')
-const fs = require('fs')
-const path = require("path")
-const exists = (filepath) => {
-  return new Promise(r=>fs.access(filepath, fs.constants.F_OK, e => r(!e)))
-}
 module.exports = {
+  version: "5.0",
   title: "InfernoSaber Automapper",
   description: "Flexible Automapper for Beatsaber made for any difficulty",
   icon: "icon.png",
-  menu: async (kernel) => {
-    let installed = await exists(path.resolve(__dirname, "install_successfull.txt"))
-    if (installed) {
-      let session = (await kernel.loader.load(path.resolve(__dirname, "session.json"))).resolved
+  menu: async (kernel, info) => {
+    const installed = info.exists("install_successfull.txt")
+    const running = {
+      install: info.running("install.json"),
+      start: info.running("start.json"),
+      update: info.running("update.json")
+    }
+
+    if (running.install) {
       return [{
-        when: "start.json",
-        on: "<i class='fa-solid fa-spin fa-circle-notch'></i> Running",
-        type: "label",
-        href: "start.json"
-      }, {
-        when: "start.json",
-        off: "<i class='fa-solid fa-power-off'></i> Launch",
-        href: "start.json?fullscreen=true&run=true",
-      }, {
-        when: "start.json",
-        on: (session && session.url ? "<i class='fa-solid fa-rocket'></i> Open Web UI" : null),
-        href: (session && session.url ? session.url : null),
-        target: "_blank"
-      }, {
-        when: "start.json",
-        on: "<i class='fa-solid fa-desktop'></i> Server",
-        href: "start.json?fullscreen=true"
-      }, {
-        html: '<i class="fa-solid fa-sync"></i> Update',
-        type: "link",
-        href: "update.json?run=true&fullscreen=true"
-      }]
-    } else {
-      return [{
-        html: '<i class="fa-solid fa-plug"></i> Install',
-        type: "link",
-        href: "install.json?run=true&fullscreen=true"
+        default: true,
+        icon: "fa-solid fa-plug",
+        text: "Installing",
+        href: "install.json"
       }]
     }
+
+    if (running.update) {
+      return [{
+        default: true,
+        icon: "fa-solid fa-terminal",
+        text: "Updating",
+        href: "update.json"
+      }]
+    }
+
+    if (installed) {
+      if (running.start) {
+        const local = info.local("start.json")
+        if (local && local.url) {
+          return [{
+            default: true,
+            icon: "fa-solid fa-rocket",
+            text: "Open Web UI",
+            href: local.url
+          }, {
+            icon: "fa-solid fa-terminal",
+            text: "Terminal",
+            href: "start.json"
+          }, {
+            icon: "fa-solid fa-sync",
+            text: "Update",
+            href: "update.json"
+          }]
+        }
+        return [{
+          default: true,
+          icon: "fa-solid fa-terminal",
+          text: "Terminal",
+          href: "start.json"
+        }]
+      }
+
+      return [{
+        default: true,
+        icon: "fa-solid fa-power-off",
+        text: "Launch",
+        href: "start.json"
+      }, {
+        icon: "fa-solid fa-sync",
+        text: "Update",
+        href: "update.json"
+      }, {
+        icon: "fa-solid fa-plug",
+        text: "Install",
+        href: "install.json"
+      }]
+    }
+
+    return [{
+      default: true,
+      icon: "fa-solid fa-plug",
+      text: "Install",
+      href: "install.json"
+    }]
   }
 }
